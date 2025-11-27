075489c63458c026767b73693b9a24ad1f7dc013
M Purushottam Naidu <129367806+purushottam897@users.noreply.github.com>
2025-11-28 00:00:23 +0530

    Change base URL for API client to port 30083

--- learning_hub_frontend/src/apiClient.js
+++ learning_hub_frontend/src/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:2074",
+  baseURL: "http://localhost:30083",
   headers: {
     "Content-Type": "application/json"
   }
